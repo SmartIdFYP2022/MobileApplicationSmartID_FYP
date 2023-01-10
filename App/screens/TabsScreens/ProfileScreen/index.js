b71589b167a4ed4d041ebe6eb242c1281b75494e
Muhammad Naveed Qamar <60569200+naveedqamar650@users.noreply.github.com>
2023-01-10 10:35:10 +0500

    Profile Page Updated

--- App/screens/TabsScreens/ProfileScreen/index.js
+++ App/screens/TabsScreens/ProfileScreen/index.js
@@ -1,12 +1,15 @@
-import * as React from 'react';
-import { Text, SafeAreaView, TouchableOpacity } from 'react-native';
-import { useTranslation } from 'react-i18next';
-import Selector from './components/Selector';
-import { FONTS } from '../../../constants';
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView, Image, ScrollView } from "react-native";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 import { replace } from '../../../navigation/RootNavigation';
 import { readData } from '../../../utils';
+import { TouchableOpacity } from 'react-native';
+import { COLORS, FONTS, } from '../../../constants';
+import {LinearGradient} from 'expo-linear-gradient';
+import { useTranslation } from 'react-i18next';
 
-const ProfileScreen = () => {
+export default function App() {
+    
     const { t, i18n } = useTranslation();
     const [email, setEmail] = React.useState('');
 
@@ -18,18 +21,195 @@ const ProfileScreen = () => {
     const onLogoutPress = () => {
         replace('welcome');
     }
+
     return (
-        <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center', }}>
-            <Text style={{ ...FONTS.h2, marginBottom:50 }}>{email}</Text>
-            <Text style={{ ...FONTS.body1 }}>
-                {t('common:languageSelector')}
-            </Text>
-            <Selector />
-            <TouchableOpacity onPress={onLogoutPress} style={{ marginTop: 100 }}>
-                <Text style={{ ...FONTS.h2, color: 'red' }}>{t('common:logout')}</Text>
+        <SafeAreaView style={styles.container}>
+            <ScrollView showsVerticalScrollIndicator={false}>
+
+                <View style={{ alignSelf: "center", marginTop: '50%' }}>
+                    <View style={styles.profileImage}>
+                        <Image source={require("../../../assets/profile-image/profile-pic.jpg")} style={styles.image} resizeMode="center"></Image>
+                    </View>
+                    <View style={styles.add}>
+                        <Ionicons name="ios-add" size={48} color="#DFD8C8" style={{ marginTop: 1, marginLeft: 4 }}></Ionicons>
+                    </View>
+                </View>
+
+                <View style={styles.infoContainer}>
+                    <Text style={[styles.text, { fontWeight: "200", fontSize: 36 }]}></Text>
+                </View>
+
+                <View style={styles.statsContainer}>
+                    <View style={styles.statsBox}>
+                        <Text style={[styles.text, { fontSize: 24 }]}>8</Text>
+                        <Text style={[styles.text, styles.subText]}>Semester</Text>
+                    </View>
+                    <View style={[styles.statsBox, { borderColor: "#DFD8C8", borderLeftWidth: 1, borderRightWidth: 1 }]}>
+                        <Text style={[styles.text, { fontSize: 24 }]}>BCSM-F19-479</Text>
+                        <Text style={[styles.text, styles.subText]}>RollNumber</Text>
+                    </View>
+                </View>
+                <Text style={[styles.subText, styles.recent]}>Recent Activity</Text>
+                <View style={{ alignItems: "center" }}>
+                    <View style={styles.recentItem}>
+                        <View style={styles.activityIndicator}></View>
+                        <View style={{ width: 250 }}>
+                            <Text style={[styles.text, { color: "#41444B", fontWeight: "300" }]}>
+                                Started following <Text style={{ fontWeight: "400" }}>Jake Challeahe</Text> and <Text style={{ fontWeight: "400" }}>Luis Poteer</Text>
+                            </Text>
+                        </View>
+                    </View>
+
+                    <View style={styles.recentItem}>
+                        <View style={styles.activityIndicator}></View>
+                        <View style={{ width: 250 }}>
+                            <Text style={[styles.text, { color: "#41444B", fontWeight: "300" }]}>
+                                Started following <Text style={{ fontWeight: "400" }}>Luke Harper</Text>
+                            </Text>
+                        </View>
+                    </View>
+                </View>
+
+                <TouchableOpacity onPress={onLogoutPress}
+                style={{
+                    marginTop: 40,
+                    width: '80%',
+                    borderRadius: 30,
+                    alignSelf: 'center'
+                }}>
+                    <LinearGradient
+                    start={{x: 0, y: 0}} end={{x: 1, y: 0}}
+                    colors={[COLORS.primary, COLORS.primary2]}
+                    style={{
+                    padding: 15, borderRadius: 20, marginBottom: 20,
+                    }}
+                >
+                    <Text style={{ ...FONTS.med4, color: COLORS.white, textAlign: 'center' }}>{t('common:logout')}</Text>
+                </LinearGradient>
             </TouchableOpacity>
+            </ScrollView>
         </SafeAreaView>
     );
 }
 
-export default ProfileScreen;
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#FFF"
+    },
+    text: {
+        fontFamily: "HelveticaNeue",
+        color: "#52575D"
+    },
+    image: {
+        flex: 1,
+        height: undefined,
+        width: undefined,
+    },
+    titleBar: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginTop: 24,
+        marginHorizontal: 16
+    },
+    subText: {
+        fontSize: 12,
+        color: "#AEB5BC",
+        textTransform: "uppercase",
+        fontWeight: "500"
+    },
+    profileImage: {
+        width: 200,
+        height: 200,
+        borderRadius: 100,
+        overflow: "hidden"
+    },
+    dm: {
+        backgroundColor: "#41444B",
+        position: "absolute",
+        top: 20,
+        width: 40,
+        height: 40,
+        borderRadius: 20,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    active: {
+        backgroundColor: "#34FFB9",
+        position: "absolute",
+        bottom: 28,
+        left: 10,
+        padding: 4,
+        height: 20,
+        width: 20,
+        borderRadius: 10
+    },
+    add: {
+        backgroundColor: "#41444B",
+        position: "absolute",
+        bottom: 0,
+        right: 0,
+        width: 60,
+        height: 60,
+        borderRadius: 30,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    infoContainer: {
+        alignSelf: "center",
+        alignItems: "center",
+        marginTop: 16
+    },
+    statsContainer: {
+        flexDirection: "row",
+        alignSelf: "center",
+        marginTop: 32
+    },
+    statsBox: {
+        alignItems: "center",
+        flex: 1
+    },
+    mediaImageContainer: {
+        width: 180,
+        height: 200,
+        borderRadius: 12,
+        overflow: "hidden",
+        marginHorizontal: 10
+    },
+    mediaCount: {
+        backgroundColor: "#41444B",
+        position: "absolute",
+        top: "50%",
+        marginTop: -50,
+        marginLeft: 30,
+        width: 100,
+        height: 100,
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 12,
+        shadowColor: "rgba(0, 0, 0, 0.38)",
+        shadowOffset: { width: 0, height: 10 },
+        shadowRadius: 20,
+        shadowOpacity: 1
+    },
+    recent: {
+        marginLeft: 78,
+        marginTop: 32,
+        marginBottom: 6,
+        fontSize: 10
+    },
+    recentItem: {
+        flexDirection: "row",
+        alignItems: "flex-start",
+        marginBottom: 16
+    },
+    activityIndicator: {
+        backgroundColor: "#CABFAB",
+        padding: 4,
+        height: 12,
+        width: 12,
+        borderRadius: 6,
+        marginTop: 3,
+        marginRight: 20
+    }
+});
